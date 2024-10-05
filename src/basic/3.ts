5136b39b1b8ad690770373cbd228e456229bfe22
Yuliia Vorobei <vorobeju1@gmail.com>
2024-10-05 15:10:41 +0200

    fist homework TS

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+type union = string | number;
+type literal = "enable" | "disable";
+// const foo: union = "Max";
+// const foo1: union = 24;
+// const foo3: union = null;
+
+// const foo4: literal = "enable";
+// const foo5: literal = "disable";
+// const foo6: literal = "Max";
---
+++ src/basic/6.ts
@@ -0,0 +1,27 @@
+const mango: IUser = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly: IUser = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
+
+interface IAddress {
+  city: string;
+  country: string;
+}
+
+interface IUser {
+  name: string;
+  age: number;
+  email: string;
+  address?: IAddress;
+}
